129e9194c31542ed18e3ff13fae83cba0b308fd1
wakkihaya <wakkihaya@gmail.com>
2021-12-16 19:35:22 +0900

    update: delete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import type { FC } from "react";
 
-const IndexPage = () => (
+import Layout from "../components/Layout";
+
+const IndexPage: FC = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
@@ -10,6 +12,6 @@ const IndexPage = () => (
       </Link>
     </p>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
