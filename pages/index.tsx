1d1025b61f33ea90f3e56e554e356b8a305b5213
wakkihaya <wakkihaya@gmail.com>
2021-12-18 16:20:23 +0900

    update: my intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,11 @@ const IndexPage: FC = () => {
   return (
     <div className="container">
       <Header />
+      <div className="container--intro">
+        My Name is Hayato! Please access to{" "}
+        <a href="https://wakkihaya.com">my website</a> if you have interests in
+        me.
+      </div>
       <PostList posts={notePosts} someType={"note"} />
       <PostList posts={qiitaPosts} someType={"Qiita"} />
       <PostList posts={mediumPosts} someType={"Medium"} />
